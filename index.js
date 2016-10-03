a3a8c01787e5e415fca7c5479dc56de31db371aa
chunyuezhang <1061940078@qq.com>
2016-10-03 16:00:07 +0800

    redirect

--- index.js
+++ index.js
@@ -22,24 +22,23 @@ db.once('open', function() {
 
 // 接收(响应)请求,而不是发出请求,浏览器(客户端)发出请求.
 // 读取所有文章
-app.get('/write',function(req,res){
-  var page ="<form method='post' action='/posts2'>"+
+app.get('/aa',function(req,res){
+  var page ="<form method='post' action='/posts'>"+
             "<input type='text' name='title'/>"+
             "<input type='submit'>"+
             "</form>"
   res.send(page)
-// res.redirect('http://baidu.com')
 })
 // 这是应该让用户看到的页面,一条路由只对应一个响应,那么问题来了...重定向
-app.get('/posts/',function(req,res){
-  // res.send('<h1>show this page</h1>')
-  // res.redireact('http://baidu.com')
+app.get('/posts', function(req, res) {
+  Post.find().sort().exec(function(err, posts) {
+      res.send(posts)
+  });
 })
 
+
 //此路由不是用来返回页面,只是用来进行数据库保存的
-app.post('/posts2/',function(req,res){
-  res.send('The Blog title is'+': ' + req.body.title)
-  console.log("GET/posts222")
+app.post('/posts/',function(req,res){
   var post = new Post({
     title:req.body.title
   })
@@ -47,6 +46,7 @@ app.post('/posts2/',function(req,res){
   post.save(function(){
     console.log('saved!');
   })
+  res.redirect('/posts')
 })
 
 
