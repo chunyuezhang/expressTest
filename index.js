6f7d2805ef75a8648267003333066764ad9a378c
chunyuezhang <1061940078@qq.com>
2016-10-03 11:56:57 +0800

    shilihua

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Post = require('./models/post');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/chunyuedb');
+
+var db = mongoose.connection;
+// 报错
+db.on('error', function() {
+  console.log('error!')
+});
+// 报成功
+db.once('open', function() {
+  console.log('success!')
+});
 
 // 接收(响应)请求,而不是发出请求,浏览器(客户端)发出请求.
 // 读取所有文章
 app.get('/write',function(req,res){
   var page ="<form method='post' action='/posts2'>"+
             "<input type='text' name='title'/>"+
-            "<input type='text' name='content'/>"+
             "<input type='submit'>"+
             "</form>"
   res.send(page)
-  console.log("GET/posts2")
+
 })
 
 //发布一篇文章
 app.post('/posts2/',function(req,res){
-  //请注意:send 只能send一次.执行上面的这条
-  res.send('The Blog content is'+': ' + req.body.content)
-  res.send('The Blog title is'+': ' + req.body.title)
-  console.log("POST/posts")
+  // res.send('The Blog title is'+': ' + req.body.title)
+console.log("GET/posts222")
+  var post = new Post({
+    title:req.body.title
+  })
+  // 异步操作,所有耗时的都执行异步操作
+  post.save(function(){
+    console.log('saved!');
+  })
 })
 
 
---
+++ models/post.js
@@ -0,0 +1,14 @@
+// 这里写专门和mongoose相关的代码
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PostSchema = new Schema(
+  {
+    title:String
+  }
+)
+
+
+
+// 这里的ost的P一定要大写,随后自动变成post,然后再加s,即posts.model相当于杯子,PostSchema定义杯子的形状
+module.exports = mongoose.model('Post',PostSchema);
