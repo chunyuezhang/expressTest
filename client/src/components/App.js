6207df8af7eac6487a56015ed15e30dad637b5ba
chunyuezhang <1061940078@qq.com>
2016-10-04 10:36:30 +0800

    server-cors

---
+++ client/src/components/App.js
@@ -0,0 +1,29 @@
+
+import React, { Component } from 'react';
+import axios from 'axios';
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      posts: "all posts"
+    };
+  }
+  componentWillMount() {
+    console.log('hello will mount');
+    axios.get('http://localhost:3000/posts', function(res){
+      this.setState({
+        posts: res.posts
+      })
+    })
+    // 再此处发 Ajax 请求，
+    // 请求服务器端的 json 数据
+  }
+  render() {
+
+    return(
+      <div>
+        { this.state.posts }
+      </div>
+    );
+  }
+}
